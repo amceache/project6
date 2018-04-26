91ddcf4c8c6e144f8a229674bf72b084e4197257
Alanna Lynn McEachen <amceache@student02.cse.nd.edu>
2018-04-26 16:48:25 -0400

    debug works except for checking indirect data blocks. format may or may not work

--- fs.c
+++ fs.c
@@ -7,11 +7,16 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
+#include <math.h>
 
 #define FS_MAGIC           0xf0f03410
 #define INODES_PER_BLOCK   128
 #define POINTERS_PER_INODE 5
 #define POINTERS_PER_BLOCK 1024
+
+struct Disk {
+	int mounted;
+};
 
 struct fs_superblock {
 	int magic;
@@ -34,9 +39,38 @@ union fs_block {
 	char data[DISK_BLOCK_SIZE];
 };
 
+/* Globals */
+static struct Disk disk;
+
 int fs_format()
 {
-	return 0;
+	// check if disk is mounted
+	if (disk.mounted) {
+		return 0;
+	}
+
+	// set up super block
+	union fs_block block;
+	block.super.magic = FS_MAGIC;
+	block.super.nblocks = disk_size();
+	// 10% of these to inodes
+	block.super.ninodeblocks = (int) disk_size()*0.1;
+	block.super.ninodes = block.super.ninodeblocks * INODES_PER_BLOCK;
+
+	// write superblock
+	disk_write(0, block.data);
+
+	// clear inode table
+	for (int i=0; i<DISK_BLOCK_SIZE; i++) {
+		block.data[i] = 0;
+	}
+
+	for (int i=1; i<disk_size(); i++) {
+		disk_write(i, block.data);
+	}
+
+
+	return 1;
 }
 
 void fs_debug()
@@ -46,13 +80,51 @@ void fs_debug()
 	disk_read(0,block.data);
 
 	printf("superblock:\n");
-	printf("    %d blocks\n",block.super.nblocks);
-	printf("    %d inode blocks\n",block.super.ninodeblocks);
-	printf("    %d inodes\n",block.super.ninodes);
+	// check if magic number valid
+	if (block.super.magic == FS_MAGIC) {
+		printf("    magic number is valid\n");
+	} else {
+		printf("    magic number is not valid\n");
+	}
+
+	printf("    %d blocks on disk\n",block.super.nblocks);
+	printf("    %d blocks for inodes\n",block.super.ninodeblocks);
+	printf("    %d inodes total\n",block.super.ninodes);
+
+	// look through inode blocks
+	for (int i=0; i<INODES_PER_BLOCK; i++) {
+		struct fs_inode inode = block.inode[i];
+		if (inode.isvalid) {
+			printf("inode %d:\n", i);
+			printf("    size: %d\n", inode.size);
+			printf("    direct blocks:");
+			for (int j=0; j<POINTERS_PER_INODE; j++) {
+				printf(" %d", inode.direct[j]);
+			}
+			printf("\n");
+			if (inode.indirect > 0) {
+				printf("    indirect block: %d\n",inode.indirect);
+				// read indirect block data
+				printf("    indirect data blocks:");
+				union fs_block ind_block;
+				disk_read(inode.indirect,ind_block.data);
+				for (int k=0; k<POINTERS_PER_BLOCK; k++) {
+					if (ind_block.pointers[k] > 0) {
+						printf(" %d", ind_block.pointers[k]);
+					}
+				}
+				printf("\n");
+			}
+		}
+	}
 }
 
 int fs_mount()
 {
+	// check if already mounted
+	if (disk.mounted) {
+		return 0;
+	}
 	return 0;
 }
 
