9f601206ac04787d4a714b2fe94a3ba16989acd7
Kaitlyn Ingram <kingram1@nd.edu>
2018-04-29 18:35:59 -0400

    additional comments

---
+++ ceil.c
@@ -0,0 +1,10 @@
+
+
+#include <math.h>
+#include <stdio.h>
+
+int main(void) 
+{
+    printf("%f\n", ceil(1.2));
+    return 0;
+}
--- fs.c
+++ fs.c
@@ -1,3 +1,7 @@
+// fs.c
+/*
+ *
+ * ************************************************************************** */
 
 #include "fs.h"
 #include "disk.h"
@@ -9,146 +13,179 @@
 #include <unistd.h>
 #include <math.h>
 
-#define FS_MAGIC           0xf0f03410
-#define INODES_PER_BLOCK   128
-#define POINTERS_PER_INODE 5
-#define POINTERS_PER_BLOCK 1024
+#define DISK_BLOCK_SIZE	    4096
+#define FS_MAGIC	    0xf0f03410
+#define INODES_PER_BLOCK    128
+#define POINTERS_PER_INODE  5 // Pointers in inode structure
+#define POINTERS_PER_BLOCK  1024 // Pointers in indirect block
 
-struct Disk {
-	int mounted;
+/* STRUCTS ------------------------------------------------------------------ */
+
+struct Disk 
+{
+    int mounted;
 };
 
-struct fs_superblock {
-	int magic;
-	int nblocks;
-	int ninodeblocks;
-	int ninodes;
+struct fs_superblock 
+{
+    int magic;
+    int nblocks;
+    int ninodeblocks;
+    int ninodes;
 };
 
-struct fs_inode {
-	int isvalid;
-	int size;
-	int direct[POINTERS_PER_INODE];
-	int indirect;
+struct fs_inode 
+{
+    int isvalid;
+    int size;
+    int direct[POINTERS_PER_INODE];
+    int indirect;
 };
 
-union fs_block {
-	struct fs_superblock super;
-	struct fs_inode inode[INODES_PER_BLOCK];
-	int pointers[POINTERS_PER_BLOCK];
-	char data[DISK_BLOCK_SIZE];
+// Represents 4 different ways of interpreting raw disk data
+union fs_block 
+{
+    struct fs_superblock super;
+    struct fs_inode inode[INODES_PER_BLOCK];
+    int pointers[POINTERS_PER_BLOCK];
+    char data[DISK_BLOCK_SIZE];
 };
 
-/* Globals */
+/* GLOBALS ------------------------------------------------------------------ */
+
 static struct Disk disk;
 
+/* FUNCTIONS ---------------------------------------------------------------- */
+
+/* creates a new filesystem on the disk, destroys data already present */
 int fs_format()
 {
-	// check if disk is mounted
-	if (disk.mounted) {
-		return 0;
-	}
+    if (disk.mounted) 
+    { // return failure if disk is mounted
+	return 0; 
+    }
 
-	// set up super block
-	union fs_block block;
-	block.super.magic = FS_MAGIC;
-	block.super.nblocks = disk_size();
-	// 10% of these to inodes
-	block.super.ninodeblocks = (int) disk_size()*0.1;
-	block.super.ninodes = block.super.ninodeblocks * INODES_PER_BLOCK;
+    // set up super block
+    union fs_block block;
+    block.super.magic = FS_MAGIC;
+    block.super.nblocks = disk_size();
+	
+    // 10% of these to inodes
+    block.super.ninodeblocks = (int) disk_size()*0.1;
+    block.super.ninodes = block.super.ninodeblocks * INODES_PER_BLOCK;
 
-	// write superblock
-	disk_write(0, block.data);
+    // write superblock
+    disk_write(0, block.data);
 
-	// clear inode table
-	for (int i=0; i<DISK_BLOCK_SIZE; i++) {
-		block.data[i] = 0;
-	}
+    // clear inode table
+    for (int i=0; i<DISK_BLOCK_SIZE; i++) 
+    {
+	block.data[i] = 0;
+    }
 
-	for (int i=1; i<disk_size(); i++) {
-		disk_write(i, block.data);
-	}
+    for (int i=1; i<disk_size(); i++) 
+    {
+	disk_write(i, block.data);
+    }
 
-
-	return 1;
+    return 1;
 }
 
+/* scans a mounted filesystem and repot on how the inodes and blocks are organized */
 void fs_debug()
 {
-	union fs_block block;
+    union fs_block block;
 
-	disk_read(0,block.data);
+    disk_read(0,block.data);
 
-	printf("superblock:\n");
-	// check if magic number valid
-	if (block.super.magic == FS_MAGIC) {
-		printf("    magic number is valid\n");
-	} else {
-		printf("    magic number is not valid\n");
-	}
+    printf("superblock:\n");
+	
+    // check if magic number valid
+    if (block.super.magic == FS_MAGIC) 
+    {
+	printf("    magic number is valid\n");
+    } 
+    else 
+    {
+	printf("    magic number is not valid\n");
+    }
 
-	printf("    %d blocks on disk\n",block.super.nblocks);
-	printf("    %d blocks for inodes\n",block.super.ninodeblocks);
-	printf("    %d inodes total\n",block.super.ninodes);
+    printf("    %d blocks on disk\n",block.super.nblocks);
+    printf("    %d blocks for inodes\n",block.super.ninodeblocks);
+    printf("    %d inodes total\n",block.super.ninodes);
 
-	// look through inode blocks
-	for (int i=0; i<INODES_PER_BLOCK; i++) {
-		struct fs_inode inode = block.inode[i];
-		if (inode.isvalid) {
-			printf("inode %d:\n", i);
-			printf("    size: %d\n", inode.size);
-			printf("    direct blocks:");
-			for (int j=0; j<POINTERS_PER_INODE; j++) {
-				printf(" %d", inode.direct[j]);
-			}
-			printf("\n");
-			if (inode.indirect > 0) {
-				printf("    indirect block: %d\n",inode.indirect);
-				// read indirect block data
-				printf("    indirect data blocks:");
-				union fs_block ind_block;
-				disk_read(inode.indirect,ind_block.data);
-				for (int k=0; k<POINTERS_PER_BLOCK; k++) {
-					if (ind_block.pointers[k] > 0) {
-						printf(" %d", ind_block.pointers[k]);
-					}
-				}
-				printf("\n");
-			}
+    // look through inode blocks
+    for (int i=0; i<INODES_PER_BLOCK; i++) 
+    {
+	struct fs_inode inode = block.inode[i];
+	if (inode.isvalid) 
+	{
+	    printf("inode %d:\n", i);
+	    printf("    size: %d\n", inode.size);
+	    printf("    direct blocks:");
+	    for (int j=0; j<POINTERS_PER_INODE; j++) 
+	    {
+		printf(" %d", inode.direct[j]);
+	    }
+	    printf("\n");
+	    if (inode.indirect > 0) 
+	    {
+		printf("    indirect block: %d\n",inode.indirect);
+		// read indirect block data
+		printf("    indirect data blocks:");
+		union fs_block ind_block;
+		disk_read(inode.indirect,ind_block.data);
+		for (int k=0; k<POINTERS_PER_BLOCK; k++) 
+		{
+		    if (ind_block.pointers[k] > 0) 
+		    {
+			printf(" %d", ind_block.pointers[k]);
+		    }
 		}
+		printf("\n");
+	    }
 	}
+    }
 }
 
+/* examine the disk for a filesystem, build a free block bitmap, prepare the filesystem for use */
 int fs_mount()
 {
-	// check if already mounted
-	if (disk.mounted) {
-		return 0;
-	}
+a   // check if already mounted
+    if (disk.mounted) {
 	return 0;
+    }
+
+    disk.mounted = true; // set mounted to tree
+    return 0;
 }
 
+/* create a new inode of zero length */
 int fs_create()
 {
-	return 0;
+    return 0;
 }
 
+/* delete the inode indicated by the number */
 int fs_delete( int inumber )
 {
-	return 0;
+    return 0;
 }
 
+/* return the logical size of of the given inode (bytes) */
 int fs_getsize( int inumber )
 {
-	return -1;
+    return -1;
 }
 
+/* read data from a valid inode */
 int fs_read( int inumber, char *data, int length, int offset )
 {
-	return 0;
+    return 0;
 }
 
+/* write data to a valie inode */
 int fs_write( int inumber, const char *data, int length, int offset )
 {
-	return 0;
+    return 0;
 }
